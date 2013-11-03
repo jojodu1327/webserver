ed19d99d0d2fd70d85a5e7c5b8a2c303855f6e38
jojodu1327 <jonathan.affriat@epitech.eu>
2013-11-03 16:34:18 -0600

    creation d'une base simple, user name password mail et de controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testappli::Application.config.secret_token = 'ca4696855e064f3a78b8b8daae2a192f88ca5ed2c42057df43878a07e27b5bcd370920119caa031a8b6cf4b31762064e4f79c6e0aaae571cbee427ae69a4fc6e'
